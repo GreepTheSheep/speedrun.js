5e271a85a85320ebb3b254cfae8c566ddf09ea08
Matthieu <89matt89.md@gmail.com>
2020-11-23 13:42:25 +0100

    Update getWR.js

--- src/rawAPI/leaderboard/getWR.js
+++ src/rawAPI/leaderboard/getWR.js
@@ -7,12 +7,12 @@
 module.exports = async function(game, category){
     const fetch = require('node-fetch')
     const SPRoptions = require('../../options.json')
-    const apiLink = SPRoptions.url + SPRoptions.apiLink + 'v' + SPRoptions.apiVersion + '/leaderboards/' + game + '/category/' + category.replace('%', '').split(' ').join('%20')
+    const apiLink = SPRoptions.url + SPRoptions.apiLink + 'v' + SPRoptions.apiVersion + '/leaderboards/' + game + '/category/' + category.replace('%', '').split(' ').join('%20') + '?top=1&embed=players'
 
     var res = await fetch(apiLink,{headers: { 'User-Agent': SPRoptions.agent }})
     res = await res.json()
     res = Object(res.data)
     
-    if (res.runs == undefined) throw "Error: Category \""+category+"\" of the game \""+game+"\" is not found"
-    return res.runs[0].run
+    if (res.status == 404) throw "Error: Category \""+category+"\" of the game \""+game+"\" is not found"
+    return res
 }
